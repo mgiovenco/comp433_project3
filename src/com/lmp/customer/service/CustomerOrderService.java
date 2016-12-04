91c74ff9f31d436ad4848731f37cca758057efb3
Muhammad Farhan Anjum Pirzada <pirzada@live.com>
2016-12-03 23:41:19 -0600

    Update CustomerOrderService.java

--- src/com/lmp/customer/service/CustomerOrderService.java
+++ src/com/lmp/customer/service/CustomerOrderService.java
@@ -3,8 +3,11 @@ package com.lmp.customer.service;
 import com.lmp.customer.model.CustomerOrderRepresentation;
 import com.lmp.customer.model.CustomerOrderRequest;
 import com.lmp.customer.model.CustomerOrderStatusRepresentation;
+import java.util.Set;
+
 import javax.ws.rs.core.Response;
 
+@WebService
 public interface CustomerOrderService {
 	public CustomerOrderRepresentation getCustomerOrder(String orderId);
 	public CustomerOrderStatusRepresentation getCustomerOrderStatus(String orderId);
